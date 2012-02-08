aefcb58df239b4eaeec8f2c3a7df7a61655d0778
ccoughlin <ccoughlin@tri-austin.com>
2012-02-08 13:05:04 -0600

    Added exception handling for out-of-memory errors in the Preview Window

--- controllers/preview_window_ctrl.py
+++ controllers/preview_window_ctrl.py
@@ -4,6 +4,7 @@ Chris R. Coughlin (TRI/Austin, Inc.)
 """
 
 from models.preview_window_model import PreviewWindowModel
+import wx
 
 __author__ = 'Chris R. Coughlin'
 
@@ -17,29 +18,34 @@ class PreviewWindowController(object):
     def load_data(self):
         """Loads the instance's data file and updates the PreviewWindow's
         spreadsheet with the contents"""
-        data = self.model.load_data()
-        if data is not None:
-            self.view.spreadsheet.ClearGrid()
-            self.view.spreadsheet.SetNumberRows(0)
-            self.view.spreadsheet.SetNumberCols(0)
-            rownum = 0
-            if data.ndim == 2:
-                num_rows = data.shape[0]
-                for row in range(num_rows):
-                    self.view.spreadsheet.AppendRows(1)
-                    numcols = data[row].size
-                    if self.view.spreadsheet.GetNumberCols() < numcols:
-                        self.view.spreadsheet.SetNumberCols(numcols)
-                    colnum = 0
-                    for cell in data[row]:
-                        self.view.spreadsheet.SetCellValue(rownum, colnum, str(cell))
-                        colnum += 1
-                    rownum += 1
-            elif data.ndim == 1:
-                self.view.spreadsheet.SetNumberCols(1)
-                for el in data:
-                    self.view.spreadsheet.AppendRows(1)
-                    self.view.spreadsheet.SetCellValue(rownum, 0, str(el))
-                    rownum += 1
-        else:
-            self.view.Destroy()
+        try:
+            data = self.model.load_data()
+            if data is not None:
+                self.view.spreadsheet.ClearGrid()
+                self.view.spreadsheet.SetNumberRows(0)
+                self.view.spreadsheet.SetNumberCols(0)
+                rownum = 0
+                if data.ndim == 2:
+                    num_rows = data.shape[0]
+                    for row in range(num_rows):
+                        self.view.spreadsheet.AppendRows(1)
+                        numcols = data[row].size
+                        if self.view.spreadsheet.GetNumberCols() < numcols:
+                            self.view.spreadsheet.SetNumberCols(numcols)
+                        colnum = 0
+                        for cell in data[row]:
+                            self.view.spreadsheet.SetCellValue(rownum, colnum, str(cell))
+                            colnum += 1
+                        rownum += 1
+                elif data.ndim == 1:
+                    self.view.spreadsheet.SetNumberCols(1)
+                    for el in data:
+                        self.view.spreadsheet.AppendRows(1)
+                        self.view.spreadsheet.SetCellValue(rownum, 0, str(el))
+                        rownum += 1
+        except MemoryError: # File too large to load
+            err_msg = "The file is too large to load."
+            err_dlg = wx.MessageDialog(self.view, message=err_msg,
+                caption="Unable To Preview Data", style=wx.ICON_ERROR)
+            err_dlg.ShowModal()
+            err_dlg.Destroy()
