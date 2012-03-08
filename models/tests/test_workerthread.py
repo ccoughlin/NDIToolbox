7c052250f88fe4734078b6f36e9c44acfd029d72
CRC@ws-ccoughlin-0.tri-inside.com servers.tri-inside.com tri.int <CRC@ws-ccoughlin-0.tri-inside.com servers.tri-inside.com tri.int>
2012-03-08 13:10:40 -0600

    Added workerthread.py (missed it last check-in)

---
+++ models/tests/test_workerthread.py
@@ -0,0 +1,45 @@
+"""test_workerthread.py - tests the workerthread module
+
+Chris R. Coughlin (TRI/Austin, Inc.)
+"""
+
+__author__ = 'Chris R. Coughlin'
+
+import unittest
+from models import workerthread
+import Queue
+
+class TestWorkerThread(unittest.TestCase):
+    """Tests the WorkerThread class"""
+
+    def sample_function(self, a, b):
+        """Sample function to test WorkerThread execution"""
+        return (a + b)
+
+    def sample_exception_function(self, a, b):
+        """Sample function to test handling Exceptions"""
+        raise Exception("An error has occurred.")
+
+    def setUp(self):
+        self.message_queue = Queue.Queue()
+        self.exception_queue = Queue.Queue()
+
+    def test_execution(self):
+        """Verify execution without Exceptions"""
+        a_thread = workerthread.WorkerThread(exception_queue=self.exception_queue, return_queue=self.message_queue,
+            target=self.sample_function, args=(1, 2))
+        a_thread.start()
+        a_thread.join()
+        self.assertEqual(self.sample_function(1, 2), self.message_queue.get())
+
+    def test_exception_execution(self):
+        """Verify Exceptions are passed back through the Exception Queue"""
+        a_thread = workerthread.WorkerThread(exception_queue=self.exception_queue, return_queue=self.message_queue,
+            target=self.sample_exception_function, args=(1, 2))
+        a_thread.start()
+        a_thread.join()
+        exc_type, exc = self.exception_queue.get()
+        self.assertTrue(isinstance(exc, Exception))
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ models/workerthread.py
@@ -0,0 +1,37 @@
+"""workerthread.py - simple Thread wrapper for running long processes
+
+Chris R. Coughlin (TRI/Austin, Inc.)
+"""
+
+__author__ = 'Chris R. Coughlin'
+
+import threading
+import sys
+
+class WorkerThread(threading.Thread):
+    """Daemon Thread wrapper to execute long-running processes"""
+
+    def __init__(self, exception_queue, return_queue=None, group=None, target=None, name=None,
+                 args=(), kwargs={}, verbose=None):
+        """Standard Thread API with the addition of Queues for passing Exceptions
+        and return valuess."""
+        threading.Thread.__init__(self, group=group, target=target, name=name, verbose=verbose)
+        self.daemon = True
+        self.target = target
+        self.args = args
+        self.kwargs = kwargs
+        self.exception_queue = exception_queue
+        self.result_queue = return_queue
+
+    def run(self):
+        """Runs the target function.  Exceptions are returned
+        in the instance's exception_queue as exception type, exception
+        tuple (sys.exc_info()[:2].  Returns the target's return value
+        if the instance's result_queue has been set."""
+        try:
+            retval = self.target(*self.args, **self.kwargs)
+            if self.result_queue is not None:
+                self.result_queue.put(retval)
+        except Exception:
+            # Pass a message to the calling thread with the Exception information
+            self.exception_queue.put(sys.exc_info()[:2])
