6be3cf041174bc53fc78160600fbf70bb1aefef4
CRC@ws-ccoughlin-0.tri-inside.com <CRC@ws-ccoughlin-0.tri-inside.com>
2012-05-10 13:27:38 -0500

    Added ndescanhandler module to return NDE "A" "B" and "C" scans from 3D data sets

---
+++ models/ndescanhandler.py
@@ -0,0 +1,66 @@
+"""ndescanhandler.py - returns A, B, and C scan datasets
+from three-dimensional NumPy arrays
+
+Chris R. Coughlin (TRI/Austin, Inc.)
+"""
+
+__author__ = 'Chris R. Coughlin'
+
+import numpy as np
+
+class NDEScanHandler(object):
+    """Utility class to return A, B, and C scan
+    arrays from three-dimensional NDE scans"""
+
+    def __init__(self, np_array):
+        """Creates the array handler from the specified
+        NumPy array.  Raises AssertionError if np_array
+        does not have three dimensions."""
+        assert np_array.ndim == 3
+        self.data = np_array
+
+    def cscan_data(self, slice_idx):
+        """Returns the 2D slice of the 3D array
+        at index z=slice_idx."""
+        return self.data[:, :, slice_idx]
+
+    def ascan_data(self, xpos, ypos):
+        """Returns the waveform from the (x,y) position"""
+        return self.data[ypos, xpos, :]
+
+    def hbscan_data(self, slice_idx, ypos):
+        """Returns the horizontal slice at y=ypos
+        through the 2D slice at z=slice_idx."""
+        # TODO: replace with single operation
+        # self.data[ypos, :, slice_idx]
+        # when initial testing complete
+        cscan_data = self.cscan_data(slice_idx)
+        return cscan_data[ypos, :]
+
+    def vbscan_data(self, slice_idx, xpos):
+        """Returns the vertical slice at x=xpos
+        through the 2D slice at z=slice_idx."""
+        # TODO: replace with single operation
+        # self.data[:, xpos, slice_idx]
+        # when initial testing complete
+        cscan_data = self.cscan_data(slice_idx)
+        return cscan_data[:, xpos]
+
+    def gen_cscan(self, start_idx, stop_idx, fn=None):
+        """Returns a computed C scan dataset:  takes the subset
+        of data between z=start_idx and z=stop_idx and applies a
+        function to return a computed 2D array.  Raises an
+        AssertionError if stop_idx <= start_idx.
+
+        Function defaults to numpy.amax if not provided and must be
+        one of the following functions that support an axis argument:
+        [np.amax, np.amin, np.ptp, np.average, np.mean, np.median].
+        """
+        assert stop_idx > start_idx
+        data = self.data[:, :, start_idx:stop_idx]
+        if fn is None:
+            cscan_data = np.amax(data, axis=2)
+        else:
+            assert fn in [np.amax, np.amin, np.ptp, np.average, np.mean, np.median]
+            cscan_data = fn(data, axis=2)
+        return cscan_data
---
+++ models/tests/test_ndescanhandler.py
@@ -0,0 +1,92 @@
+"""test_ndescanhandler.py - tests the ndescanhandler module
+
+Chris R. Coughlin (TRI/Austin, Inc.)
+"""
+
+__author__ = 'Chris R. Coughlin'
+
+from models import ndescanhandler
+import numpy as np
+import random
+import unittest
+class TestNDEScanHandler(unittest.TestCase):
+    """Tests the NDEScanHandler class"""
+
+    def setUp(self):
+        self.threed_array = self.random3D_data()
+        self.min_x = 0
+        self.max_x = self.threed_array.shape[1] - 1
+        self.min_y = 0
+        self.max_y = self.threed_array.shape[0] - 1
+        self.min_z = 0
+        self.max_z = self.threed_array.shape[2] - 1
+        self.scnr = ndescanhandler.NDEScanHandler(self.threed_array)
+
+    def random3D_data(self):
+        """Generates a random 3D array of data"""
+        raw_array = np.array([random.uniform(-100, 100) for i in range(24)])
+        three_d_array = raw_array.reshape((3, 2, 4))
+        return three_d_array
+
+    def test_dimcheck(self):
+        """Verify NDEScanHandler rejects arrays that are not 3D"""
+        oned_arr = np.zeros([5])
+        with self.assertRaises(AssertionError):
+            scnr = ndescanhandler.NDEScanHandler(oned_arr)
+
+    def test_cscan_data(self):
+        """Verify returning a single 2D slice from a 3D array"""
+        random_slices = [random.uniform(0, self.threed_array.shape[2]) for i in range(2)]
+        for slice_idx in random_slices:
+            expected_array = self.threed_array[:, :, slice_idx]
+            returned_slice = self.scnr.cscan_data(slice_idx)
+            self.assertListEqual(expected_array.tolist(), returned_slice.tolist())
+
+    def test_ascan_data(self):
+        """Verify returning a complete waveform from a given (x,y) position"""
+        for i in range(10):
+            xpos = random.randint(self.min_x, self.max_x)
+            ypos = random.randint(self.min_y, self.max_y)
+            expected_waveform = self.threed_array[ypos, xpos, :]
+            returned_waveform = self.scnr.ascan_data(xpos, ypos)
+            self.assertListEqual(expected_waveform.tolist(), returned_waveform.tolist())
+
+    def test_horizontalbscan_data(self):
+        """Verify returning the horizontal B Scan from the C Scan
+        (1D slice at constant y from the 2D NumPy array)."""
+        slice_idx = random.randint(self.min_z, self.max_z)
+        for ypos in range(self.min_y, self.max_y):
+            expected_slice = self.threed_array[ypos, :, slice_idx]
+            returned_bscan = self.scnr.hbscan_data(slice_idx, ypos)
+            self.assertListEqual(expected_slice.tolist(), returned_bscan.tolist())
+
+    def test_verticalbscan_data(self):
+        """Verify returning the vertical B Scan from the C Scan
+        (1D slice at constant x from the 2D NumPy array)."""
+        slice_idx = random.randint(self.min_z, self.max_z)
+        for xpos in range(self.min_x, self.max_x):
+            expected_slice = self.threed_array[:, xpos, slice_idx]
+            returned_bscan = self.scnr.vbscan_data(slice_idx, xpos)
+            self.assertListEqual(expected_slice.tolist(), returned_bscan.tolist())
+
+    def test_gen_cscan(self):
+        """Verify returning a 2D array based on a supplied
+        operation"""
+        start_idx = random.randint(self.min_z, self.max_z)
+        stop_idx = random.randint(self.min_z, self.max_z)
+        if stop_idx < start_idx:
+            tmp_idx = start_idx
+            start_idx = stop_idx
+            stop_idx = tmp_idx
+        elif stop_idx == start_idx:
+            start_idx = 0
+            stop_idx = self.max_z
+        available_ops = [np.amax, np.amin, np.ptp, np.average, np.mean, np.median]
+        for op in available_ops:
+            expected_result = op(self.threed_array[:, :, start_idx:stop_idx], axis=2)
+            returned_result = self.scnr.gen_cscan(start_idx, stop_idx, op)
+            self.assertListEqual(expected_result.tolist(), returned_result.tolist())
+
+if __name__ == "__main__":
+    random.seed()
+    unittest.main()
